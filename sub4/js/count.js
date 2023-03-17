61e975886af2b2a28999322ec1adf2ee6fe8dfd2
min <earnestga@naver.com>
2023-03-17 16:44:22 +0900

    카운트 이벤트 수정 및 리드미 업데이트

--- sub4/js/count.js
+++ sub4/js/count.js
@@ -1,8 +1,30 @@
 /*숫자 자동입력*/
-
 // 전력
-//var memberCountConTxt= [134,120,100];
-//var count = $(".result ul li span:eq(1)").
+let result = document.querySelector(".result img");
+let count = document.querySelector(".result ul");
+// let resultScorllHeight = window.pageYOffset + result.getBoundingClientRect().top + 150;
+let resultScorllHeight = result.offsetTop + 100;
+let countScrollHeight = count.offsetTop;
+
+ window.addEventListener("scroll", scrollEvent)
+
+ function scrollEvent() {
+  var scroll = $(window).scrollTop(); //스크롤 top의 높이
+  //$(".topText").text(scroll); //스크롤 좌표의 값
+  let onoff = false;
+
+  if (scroll > resultScorllHeight && scroll <= countScrollHeight) {
+    count1();
+    onoff = true;
+  }
+
+  if (onoff == true) {
+    window.removeEventListener("scroll",scrollEvent);
+  }
+};
+
+function count1 () {
+//전력 사용량 감소
 var memberCountConTxt = 134;
 
 $({ val: 0 }).animate(
@@ -19,6 +41,7 @@ $({ val: 0 }).animate(
     },
   }
 );
+
 
 // 온실가스
 memberCountConTxt = 120;
@@ -42,6 +65,7 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "");
 }
 
+
 //용수
 memberCountConTxt = 100;
 
@@ -59,3 +83,4 @@ $({ val: 0 }).animate(
     },
   }
 );
+}
