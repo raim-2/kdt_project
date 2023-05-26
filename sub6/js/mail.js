efad008f966f83173cec65a30f4ae5b9765d6e9f
min <earnestga@naver.com>
2023-05-27 02:36:35 +0900

    edit mail.php

--- sub6/js/mail.js
+++ sub6/js/mail.js
@@ -1,42 +1,33 @@
-// var uploadFile = document.getElementById('file');
-var uploadBox = document.querySelector('.uploadBox dl')
-var uploadInput = document.querySelector('.uploadBox input')
-var uploadData = document.querySelector('.uploadBox dd')
+let uploadBox = document.querySelector('.uploadBox dl')
+let uploadInput = document.querySelector('.uploadBox input')
+let uploadData = document.querySelector('.uploadBox dd')
+let imgPreview = document.querySelector('.imgPreview');
 
 if(uploadInput.files.length === 0) {
     let uploadResult = document.createElement('span');
-    uploadResult.innerText = '선택된 파일이 없습니다.';
-    uploadResult.classList = 'nofile';
+    uploadResult.innerText = '*선택된 파일이 없습니다. 이미지 형식만 가능합니다.';
+    uploadResult.className = 'nofile';
     uploadData.appendChild(uploadResult);
-    console.log('선택된 파일 없음');
 }
 
 uploadInput.addEventListener('change', updateFile);
 
 function updateFile(e) {
-    var uploadFile = e.target.files; // FileList 객체
-    console.log(uploadFile); // { 0: File, 1: File, length: 2 }
-    console.log(uploadFile[0]); //업로드된 파일의 정보를 보여줌
+    let uploadFile = e.target.files; // FileList 객체
+     console.log(uploadFile); // { 0: File, 1: File, length: 2 }
+     console.log(uploadFile[0]); //업로드된 파일의 정보를 보여줌
     
     if(uploadFile.length !== 0) {
         let lastChildText = uploadData.lastChild.innerText;
-        if(lastChildText == '선택된 파일이 없습니다.' || lastChildText =='유효한 파일 형식이 아닙니다.') {
+        if(lastChildText == '*선택된 파일이 없습니다. 이미지 형식만 가능합니다.' || lastChildText =='유효한 파일 형식이 아닙니다.') {
             uploadData.removeChild(uploadData.lastChild);
         } 
-
         fileReader(e);
-        // if (lastChildClass == 'imgPreview') {
-        //     uploadData.removeChild(uploadData.lastChild);
-        // }
     }
-
-    // fileReader(e);
 }
 
 function fileReader(e) {
-    var uploadFile = e.target.files; // FileList 객체
-    console.log(uploadFile); // { 0: File, 1: File, length: 2 }
-    console.log(uploadFile[0]); //업로드된 파일의 정보를 보여줌
+    let uploadFile = e.target.files; // FileList 객체
 
     //validFileType() 매개변수로 file객체 받아 type이 fileTypes 값과 동일한지 판단
     const fileTypes = [
@@ -53,29 +44,34 @@ function fileReader(e) {
     ];
 
     function validFileType(file) {
-    console.log(file,file[0].type)
     return fileTypes.includes(file[0].type);
     }
 
     //파일리더 api
-    var fileReader = new FileReader(); //객체 
-    fileReader.readAsDataURL(e.target.files[0]); //주소로 반환해라
+    let fileReader = new FileReader(); //객체 
+
+    //주소 반환해라_이미지 -> base64로 인코딩(클라이언트 측에서만)
+    fileReader.readAsDataURL(e.target.files[0]); 
     fileReader.onload = function(e) { 
-        console.log(e.target.result);
-        if(validFileType(uploadFile)) {
-        let imgPreview = document.createElement('div');
-        imgPreview.classList = 'imgPreview';
-        uploadData.appendChild(imgPreview);
+        if (imgPreview.children.length == 1 ) {
+            imgPreview.classList.remove('on')
+            imgPreview.classList.add('off') 
+            imgPreview.removeChild(imgPreview.firstChild);
+        }
         
-        let imgInList = document.createElement('img');
-        imgInList.src = e.target.result;
-        imgInList.alt = '첨부파일 이미지 미리보기';
-        console.log(imgInList.src);
-        imgPreview.appendChild(imgInList);
+        if(validFileType(uploadFile)) {
+            let imgInList = document.createElement('img');
+            imgInList.src = e.target.result;
+            imgInList.alt = '첨부파일 이미지 미리보기';
+            imgPreview.appendChild(imgInList);
+
+            imgPreview.classList.remove('off')
+            imgPreview.classList.add('on') 
         } else {
-        let uploadResult = document.createElement('span');
-        uploadResult.innerText ='유효한 파일 형식이 아닙니다.';
-        uploadData.appendChild(uploadResult);
+            let uploadResult = document.createElement('span');
+            uploadResult.innerText ='유효한 파일 형식이 아닙니다.';
+            uploadResult.className = 'invalidType';
+            uploadData.appendChild(uploadResult);
         }
     } 
 }
--- sub6/mail.php
+++ sub6/mail.php
@@ -7,29 +7,89 @@
  $phone_03=$_POST['tel'];
  $type_04=$_POST['type'];
  $msg_05=$_POST['message'];
- $file_06=$_FILES['file'];
+ $file_06=$_FILES['file']; //전달될 때 이진형태로 전달
  $agree_07=$_POST['agree'];
- 
+
+ $file_name=$_FILES['file']['name'];
+ $file_size=$_FILES['file']['size'];
+ $file_type=$_FILES['file']['type'];
+ $file_tmp=$_FILES['file']['tmp_name']; //임시명 - 임시경로(서버 저장전)
+
+ // 이미지 파일을 이동시킬 경로
+ $target_path = './data/'.$file_name;
+ //$image_path = 'http://earnestga3.cafe24.com/sub6/data/' . $file_name;
+ // move_uploaded_file(임시경로,목적지)- 임시경로 사용은 보안을 위해
+ move_uploaded_file($file_tmp, $target_path); 
+ //move_uploaded_file($file_tmp, $image_path ); 
+
+ // 이미지를 base64로 인코딩
+ $encoded_image = base64_encode(file_get_contents($target_path));
+ //$encoded_image = base64_encode(file_get_contents($image_path));
+ //$file_data = chunk_split($encoded_image); //지정 길이마다 줄바꿈 처리
+ $image_tag = '<img src="data:' . $file_type . ';base64,' . $encoded_image . '" alt="첨부파일 이미지" name="'.$file_name.'">';
+ //$image_tag = '<img src="' . $image_path . '" alt="첨부파일 이미지">';
+
  $to='earnestga@naver.com'; //master mail
  $subject='동아ST사이트에서 관리자에게 보낸 메일';
- $msg="보낸사람:$name_01\n".
-      "보낸사람메일주소:$mail_02\n".
-      "보낸사람전화번호:$phone_03\n".
-      "문의유형:$type_04\n".
-      "내용:$msg_05\n".
-      "첨부파일:$file_06\n".
-      "개인정보 수집 및 이용동의:$agree_07";
-      
- 
- mail($to,$subject,$msg,'보낸사람메일주소:'.$mail_02);   
+ $msg="보낸사람: $name_01<br>".
+      "보낸사람 메일주소: $mail_02<br>".
+      "보낸사람 전화번호: $phone_03<br>".
+      "문의유형: $type_04<br>".
+      "내용: $msg_05<br>".
+      "개인정보 수집 및 이용동의: $agree_07<br>";
+
+$msg_file="<hr>첨부파일 이름:{$file_name}<br>".
+        "첨부파일 크기:{$file_size}<br>".
+        "첨부파일 사이즈:{$file_type}<br>";
+
+$attachment="{$image_tag}";
+$boundary = md5(uniqid(microtime()));
+
+
+
+if ($file_size > 0) {
+   $headers = "MIME-Version: 1.0\r\n";
+   $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+
+   $bodytext = "--$boundary\r\n";
+   $bodytext .= "Content-Type: text/html; charset=UTF-8\r\n";
+   $bodytext .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
+   $bodytext .= $msg . "\r\n\r\n";
+   $bodytext .= $msg_file . "\r\n\r\n";
+   $bodytext .= "--$boundary\r\n";
+   $bodytext .= "Content-Type: text/html; charset=UTF-8\r\n";
+   $bodytext .= "Content-Transfer-Encoding: base64\r\n";
+   $bodytext .= "Content-Disposition: attachment; filename=\"$file_name\"\r\n\r\n";
+   $bodytext .= $attachment . "\r\n";
+   $bodytext .= "--$boundary--";
+//    $bodytext .= "Content-Type: $file_type; filename=\"$file_name\"\r\n\r\n";
+//    $bodytext .= "Content-Transfer-Encoding: base64\r\n";
+//    $bodytext .= "Content-Disposition: attachment; filename=\"$file_name\"\r\n\r\n";
+//    $bodytext .= $encoded_image . "\r\n";
+//    $bodytext .= $attachment . "\r\n";
+//    $bodytext .= "--$boundary--";
+} else {
+   $headers = "MIME-Version: 1.0\r\n";
+   $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+   $headers .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
+
+   $bodytext = $msg;
+}
+
+
+ mail($to, $subject, $bodytext, $headers, $mail_02);   
+//   var_dump($target_path);
+//   var_dump($encoded_image);
+//   var_dump($image_tag);
+//   var_dump($attachment);
+//print_r($file_06)
 
 echo "<script>
         alert('성공적으로 메일이 전송되었습니다.');
         //history.go(-1);
-        location.href='../index.html' ;
+        location.href='./sub6_3.html' ;
 </script>
 "
- 
 
  /*
  echo '이름:'.$name_01.'<br />';
@@ -39,6 +99,5 @@ echo "<script>
  echo '메일이 성공적으로 전송되었습니다<br />';
  */
 ?>
-
 
 
