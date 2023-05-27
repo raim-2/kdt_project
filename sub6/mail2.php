d617555efa0eefdf16ab59b06f98069b4c836490
min <earnestga@naver.com>
2023-05-27 22:03:38 +0900

    php mail() 사용해 첨부파일 적용

--- sub6/mail2.php
+++ sub6/mail2.php
@@ -1,4 +1,4 @@
-<meta charset="UTF-8">
+﻿<meta charset="UTF-8">
 
 <?php
 
@@ -20,22 +20,21 @@ if ($file_type == "") {
     $file_type = 'application/octet-stream';
 }
 
-//별도로 data 파일에 이미지 저장 안됨
-$filename = basename($file_name);
-$fp = fopen($file_tmp, "r"); // 임시 경로($file_tmp)를 사용합니다.
-$file = fread($fp, $file_size);
-fclose($fp);
+// $filename = basename($file_name);
+// $fp = fopen($file_tmp, "r"); // 임시 경로($file_tmp)를 사용합니다.
+// $file = fread($fp, $file_size);
+// fclose($fp);
 
-$attach = chunk_split(base64_encode($file));
+// $attach = chunk_split(base64_encode($file));
 
- // 이미지 파일을 이동시킬 경로
- //$target_path = './data/'.$file_name;
+ // 이미지 파일을 이동시킬 경로 - data 파일에 이미지 저장됨
+ $target_path = './data/'.$file_name;
  // move_uploaded_file(임시경로,목적지)- 임시경로 사용은 보안을 위해
- //move_uploaded_file($file_tmp, $target_path); 
+ move_uploaded_file($file_tmp, $target_path); 
 
  // 이미지를 base64로 인코딩
- //$encoded_image = base64_encode(file_get_contents($target_path));
- //$image_tag = '<img src="data:' . $file_type . ';base64,' . $encoded_image . '" alt="첨부파일 이미지">';
+ $encoded_image = base64_encode(file_get_contents($target_path));
+ $image_tag = '<img src="data:' . $file_type . ';base64,' . $encoded_image . '" alt="첨부파일 이미지">';
 
  $to='earnestga@naver.com'; //master mail
  $subject='동아ST사이트에서 관리자에게 보낸 메일';
@@ -62,10 +61,16 @@ if ($file_tmp != "") {
    $bodytext .= $msg . "\r\n\r\n";
    $bodytext .= $attach_msg . "\r\n\r\n";
    $bodytext .= "--$boundary\r\n";
-   $bodytext .= "Content-Type: $file_type; name=\"$file_name\"\r\n";
-   $bodytext .= "Content-Transfer-Encoding: base64\r\n";
-   $bodytext .= "Content-Disposition: attachment; filename=\"$file_name\"\r\n\r\n";
-   $bodytext .= $attach . "\r\n";
+   // $bodytext .= "Content-Type: $file_type; name=\"$file_name\"\r\n";
+   // $bodytext .= "Content-Transfer-Encoding: base64\r\n";
+   // 아래 코드가 있어야 메일에서 다운받는 첨부파일 형태로 보여줌 
+   // $bodytext .= "Content-Disposition: attachment; filename=\"$file_name\"\r\n\r\n"; 
+   // $bodytext .=  $encoded_image . "\r\n";
+   $bodytext .= "Content-Type: text/html; charset=UTF-8\r\n";
+   $bodytext .= "Content-Transfer-Encoding: 8bit\r\n\r\n"; // 이거까지 있어야 html형태로 전달해줌
+   // $bodytext .= "Content-Transfer-Encoding: base64\r\n"; // html 형식으로 작성해서 얘로 작성하면, 이미지가 안뜸(메일은 옴)
+   // $bodytext .= "Content-Disposition: attachment; filename=\"$file_name\"\r\n\r\n"; //html형태로 작성했다는 것과 같이 쓰면 오류 - 메일안옴
+   $bodytext .=  $image_tag . "\r\n";
    $bodytext .= "--$boundary--";
 } else {
    $headers = "MIME-Version: 1.0\r\n";
@@ -77,11 +82,18 @@ if ($file_tmp != "") {
 
  mail($to, $subject, $bodytext, $headers, $mail_02);   
 //  var_dump($attach);
+var_dump($file_name);
+var_dump($image_tag);
+var_dump($attach_msg);
+var_dump($msg);
 
 echo "<script>
         alert('성공적으로 메일이 전송되었습니다.');
         //history.go(-1);
-        location.href='./sub6_3.html' ;
+        //location.href='./sub6_3.html' ;
 </script>
 "
 ?>
+
+
+
